92ff31a0e55bb134c87dbc69126a714721233518
Wyatt Ramsey <61995086+JesusCheezits@users.noreply.github.com>
2021-07-26 18:49:23 -0700

    Time: 92 ms (51.70%), Space: 47 MB (26.94%) - LeetHub

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -2,12 +2,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+//     var res = false; //true if there is a copy
+//     for (let i = 0; i < nums.length; i ++) {
+//       if (nums.slice(i + 1).includes(nums[i])) {
+//         res = true;
+//       }
+//     }
+//     return res;
+// };
+
+
+// var containsDuplicate = function(nums) {
+//     return new Set(nums).size < nums.length;
+// };
+
 var containsDuplicate = function(nums) {
-    var res = false; //true if there is a copy
-    for (let i = 0; i < nums.length; i ++) {
-      if (nums.slice(i + 1).includes(nums[i])) {
-        res = true;
-      }
+   var obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if( obj[nums[i]] > 1) return true;
     }
-    return res;
+    return false;
 };
