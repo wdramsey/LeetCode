6f22b56238872ff7849af2904a3acc1619b838a1
Wyatt Ramsey <61995086+JesusCheezits@users.noreply.github.com>
2021-07-26 19:07:53 -0700

    Time: 100 ms (60.32%), Space: 49.2 MB (12.58%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,22 +3,30 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let res = 0;
-    let temp = 0;
-    let copy = prices.slice();
-    let max = Math.max(...copy);
+//     let res = 0;
+//     let temp = 0;
+//     let max = Math.max(...prices);
   
-    if (prices.length === 0) {
-      return 0;
-    }
-    for (let i = 0; i < copy.length; i++) {
-      if (res < max - copy[i]) {
-        max = Math.max(...copy.slice(i + 1))
-        temp =  max - copy[i];
-        if (temp > res) {
-          res = temp;
-        }
-      }
+//     if (prices.length === 0) {
+//       return 0;
+//     }
+//     for (let i = 0; i < prices.length; i++) {
+//       if (res < max - prices[i]) {
+//         if (prices.indexOf(max) < i) {
+//           max = Math.max(...prices.slice(i + 1));
+//         }
+//         temp =  max - prices[i];
+//         if (temp > res) {
+//           res = temp;
+//         }
+//       }
+//     }
+//     return res;
+    let res = 0;
+    let min  = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        res = Math.max(res, prices[i] - min);
     }
     return res;
 };
