1c72cad435224b3e409f19e2fc8407073f0bd8e3
Wyatt Ramsey <61995086+JesusCheezits@users.noreply.github.com>
2021-11-09 19:28:03 -0800

    Time: 72 ms (78.01%), Space: 38.5 MB (65.37%) - LeetHub

--- house-robber/house-robber.js
+++ house-robber/house-robber.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var rob = function(nums) {
-   if (nums.length == 0) return 0;
-    let prev1 = 0;
-    let prev2 = 0;
-    let temp;
-    for (let i = 0; i < nums.length; i++) {
-        tmp = prev1;
-        prev1 = Math.max(prev2 + nums[i], prev1);
-        prev2 = tmp;
+    if (nums.length === 1) return nums[0];
+    const table = Array(nums.length + 1).fill(0);
+    table[1] = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        var val = nums[i];
+        table[i + 1] = Math.max(table[i], val + table[i - 1]);
     }
-    return prev1;
-};
+    return table[table.length - 1];
+}
