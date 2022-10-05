133b44644ce0124d577d65e010d3dfec65c6130d
Wyatt Ramsey <wyattdramsey@gmail.com>
2022-10-04 17:56:46 -0700

    Time: 37 ms (24.35%), Space: 13.6 MB (6.21%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if (len(nums) == 1): return nums[0]
+        table = [0]*(len(nums) + 1)
+        table[1] = nums[0]
+        for index in range(1, len(nums)):
+            current = nums[index]
+            table[index + 1] = max(table[index], current + table[index - 1])
+        return table[len(table)-1]
