edccb004dd044091683ad3d8c7442ad4eb5aecc4
Wyatt Ramsey <wyattdramsey@gmail.com>
2023-02-27 17:49:16 -0800

    Time: 558 ms (5.06%), Space: 43.6 MB (15.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> smap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i< t.length(); i++) {
+            int count = smap.getOrDefault(t.charAt(i), 0);
+            if (count == 0) return false;
+            else smap.put(t.charAt(i), count - 1);
+            System.out.println(smap);
+        }
+        return true;
+    }
+}
+
+        
