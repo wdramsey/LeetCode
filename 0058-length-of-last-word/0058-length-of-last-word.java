534f77d6867c00ae92fd1f77f064d1425546f7b0
Wyatt Ramsey <wyattdramsey@gmail.com>
2023-07-11 18:56:10 -0700

    Time: 1 ms (44.34%), Space: 40.7 MB (74.33%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String [] temp = s.split(" ");
-        return temp[temp.length - 1].length();
+        if (s.charAt(s.length() - 1) == ' ') {
+            String [] temp = s.split(" ");
+            return temp[temp.length - 1].length();
+        } else return s.length() - 1 - s.lastIndexOf(" ");
+        
     }
 }
