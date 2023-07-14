06dd52874ed6f2331c4385f92314e6e84caba468
Wyatt Ramsey <wyattdramsey@gmail.com>
2023-07-13 18:54:30 -0700

    Time: 622 ms (5.04%), Space: 44 MB (62.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,7 +4,6 @@ class Solution {
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == nums[nums.length - 1]) break;
             if (nums[i] == nums[i + 1]) {
-                System.out.println("count" + count);
                 for (int j = i + 1; j < nums.length - 1; j++) {
                     nums[j] = nums[j + 1];
                 }
